cde9e5989e903d6980844c6d083ecf6b81523682
trodevel <trodevel@gmail.com>
2024-07-19 02:26:10 +0200

    added create_package.sh

---
+++ create_package.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+<<'COMMENT'
+
+Create Package
+
+Copyright (C) 2024 Dr. Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+COMMENT
+
+#<hb>***************************************************************************
+#
+# create_package.sh <package_config> <output_file>
+#
+# package_config       - config file to create a package
+#
+# output_file          - name of the output file
+#
+# Example: create_package.sh config.cfg package.tar.gz
+#
+#<he>***************************************************************************
+
+show_help()
+{
+    sed -e '1,/^#<hb>/d' -e '/^#<he>/,$d' $0 | cut -c 3-
+}
+
+CFG=$1
+FL_OUT=$2
+
+[[ -z "$CFG" ]]    && show_help && exit;
+[[ -z "$FL_OUT" ]] && show_help && exit;
+[[ ! -f "$CFG" ]]  && { echo "ERROR: package config does not exist"; exit 1; }
+
+echo "DEBUG: config = $CFG"
+echo "DEBUG: output = $FL_OUT"
+
+source "$CFG"
+
+src=$1
+dest=$2
+
+[[ -z "$NAME" ]]  &&  { echo "ERROR: package name NAME in not defined in config $CFG"; exit 1; }
+[[ -z "$INPUT" ]] &&  { echo "ERROR: input folder INPUT in not defined in config $CFG"; exit 1; }
+
+test -d "$INPUT" || { echo "ERROR: source directory doesn't exit"; exit 1; }
+
+tar -zcvf $FL_OUT -C $INPUT . --transform "s/^\./$NAME/"
