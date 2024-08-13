ec7ce60a7a268f5b8ea51a29ad83be820dcaa5cc
trodevel <trodevel@gmail.com>
2024-08-13 02:12:00 +0200

    impr: new rollout config (#2)
    * intermediate results
    
    * added samples
    
    * intermediate results
    
    * added additional check of input files
    
    * got rid of verbose flag
    
    * added samples

--- rollout.sh
+++ rollout.sh
@@ -2,82 +2,83 @@
 
 # $Revision: 11451 $ $Date:: 2019-05-16 #$ $Author: serge $
 
-USER=$1
-HOST=$2
+<<'COMMENT'
 
-CONFIG=./rollout.cfg
+Rollout
+
+Copyright (C) 2024 Dr. Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+COMMENT
+
+#<hb>***************************************************************************
+#
+# rollout.sh <rollout_config> <destination_user_host>
+#
+# rollout_config        - config file to create a package
+#
+# destination_user_host - name of the output file
+#
+# Example: rollout.sh config.cfg package.tar.gz
+#
+#<he>***************************************************************************
 
 show_help()
 {
-    echo "Usage: rollout.sh USER HOST"
+    sed -e '1,/^#<hb>/d' -e '/^#<he>/,$d' $0 | cut -c 3-
 }
 
-if [ -z "$USER" ]
-then
-    show_help
-    exit
-fi
+CONFIG=$1
+DEST_USER_HOST=$2
 
-if [ -z "$HOST" ]
-then
-    show_help
-    exit
-fi
+[[ -z "$CONFIG" ]]    && show_help && exit;
+[[ -z "$DEST_USER_HOST" ]] && show_help && exit;
+[[ ! -f "$CONFIG" ]]  && { echo "ERROR: rollout config $CONFIG does not exist"; exit 1; }
 
-USERHOST=$USER@$HOST
-
-echo "host          = $HOST"
-echo "userhost      = $USERHOST"
-#exit
-
-if [ ! -f $CONFIG ]
-then
-    echo "ERROR: config file $CONFIG not found"
-    exit
-fi
+echo "DEBUG: config = $CONFIG"
+echo "DEBUG: output = $DEST_USER_HOST"
 
 source $CONFIG
 
-[[ -z "$PACKAGE" ]]          && echo "ERROR: \$PACKAGE is not defined in the config file" && exit
-[[ -z "$PACKAGE_FILES" ]]    && echo "ERROR: \$PACKAGE_FILES is not defined in the config file" && exit
-[[ -z "$PACKAGE_DEST_DIR" ]] && echo "ERROR: \$PACKAGE_DEST_DIR is not defined in the config file" && exit
+[[ -z "$PACKAGE" ]]          && { echo "ERROR: NAME is not defined in the config file $CONFIG"; exit 1; }
+[[ -z "$PACKAGE" ]]          && { echo "ERROR: PACKAGE is not defined in the config file $CONFIG"; exit 1; }
+[[ -z "$PACKAGE_DEST_DIR" ]] && { echo "ERROR: PACKAGE_DEST_DIR is not defined in the config file $CONFIG"; exit 1; }
+
+echo "DEBUG: name             = $NAME"
+echo "DEBUG: package          = $PACKAGE"
+echo "DEBUG: package dest dir = $PACKAGE_DEST_DIR"
+echo "DEBUG: post actions     = $PACKAGE_POST_ACTIONS"
+echo
 
 DATUM=$(date -u +%Y%m%d_%H%M)
-NAME=${PACKAGE}_$DATUM
-ARCNAME=$NAME.tar.gz
+ARCNAME=${NAME}_${DATUM}.tar.gz
 
-source $CONFIG             # need to include config file 2 times in order to pass $NAME, etc.
-
-echo "package       = $PACKAGE"
-echo "name          = $NAME"
-echo
-echo "package files = $PACKAGE_FILES"
-echo "name          = $PACKAGE_DEST_DIR"
-echo "post actions  = $PACKAGE_POST_ACTIONS"
-echo
-
-tar cfvz $ARCNAME $PACKAGE_FILES --transform "s,^,$PACKAGE/,"
-
-scp $ARCNAME $USERHOST:$PACKAGE_DEST_DIR
+scp $PACKAGE $DEST_USER_HOST:$PACKAGE_DEST_DIR/$ARCNAME
 error=$?
 
 #echo error=$error
 
-if [ $error -ne "0" ]
-then
-    echo "ERROR: scp failed, error code $error"
-    exit
-fi
+[[ $error -ne 0 ]] && { echo "ERROR: scp failed, error code $error, file $ARCNAME, host $DEST_USER_HOST"; exit 1; }
 
 #exit
 
-ssh $USERHOST "\
+ssh $DEST_USER_HOST "\
 if [ ! -d $PACKAGE_DEST_DIR ]; then echo 'ROLLOUT: creating $PACKAGE_DEST_DIR'; mkdir $PACKAGE_DEST_DIR; else echo 'ROLLOUT: destination directory $PACKAGE_DEST_DIR exists'; fi; \
 cd $PACKAGE_DEST_DIR; \
-if [ -d $PACKAGE.prev ]; then echo 'ROLLOUT: removing existing previous package'; rm -rf $PACKAGE.prev; fi; \
-if [ -d $PACKAGE ]; then echo 'ROLLOUT: package with the same name EXISTS, renaming to previous'; mv $PACKAGE $PACKAGE.prev; else echo 'ROLLOUT: package is NEW'; fi; \
-tar xfvz $ARCNAME; \
+if [ -d $NAME.prev ]; then echo 'ROLLOUT: removing existing previous package'; rm -rf $NAME.prev; fi; \
+if [ -d $NAME ]; then echo 'ROLLOUT: package with the same name EXISTS, renaming to previous'; mv $NAME $NAME.prev; else echo 'ROLLOUT: package is NEW'; fi; \
+tar xfz $ARCNAME; \
 $PACKAGE_POST_ACTIONS \
 "
-
-rm $ARCNAME
